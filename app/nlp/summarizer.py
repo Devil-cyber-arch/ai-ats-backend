4563cef74cf80cab9ccaed1d850e87de178d11a6
Devil-cyber-arch <byh8phkd67@privaterelay.appleid.com>
2026-05-12 19:14:09 +0530

    remove heavy ai models

--- app/nlp/summarizer.py
+++ app/nlp/summarizer.py
@@ -1,19 +1,2 @@
-from transformers import pipeline
-
-summarizer = pipeline(
-    "summarization",
-    model="facebook/bart-large-cnn"
-)
-
 def generate_summary(text):
-
-    text = text[:2000]
-
-    summary = summarizer(
-        text,
-        max_length=60,
-        min_length=20,
-        do_sample=False
-    )
-
-    return summary[0]["summary_text"]
+    return text[:300]
